05b620f4592df5be5eec0bfb16eb112585a86ca7
Shreya Mohan <shreyamohan@shreyas-mbp.myfiosgateway.com>
2022-04-07 18:08:07 -0400

    Shreya Mohan code for code review. To be merged to DEV_Shreya.

---
+++ HW03_Shreya_Mohan_dictionary.py
@@ -0,0 +1,38 @@
+import random
+import os
+
+# from HW06_Shreya_Mohan_utility import Utility, file_transfer
+import HW06_Shreya_Mohan_utility as util
+
+
+class Dictionary:
+
+    def __init__(self):
+        self.words = []
+        self.utils = util.Utility()
+
+    def __str__(self):
+        return f'Dictionary: ({self.words}, {self.utils})'
+
+# this function was used to read words from the .txt file
+    def file_reader(self):
+        self.words.clear()
+        path = os.getcwd()+"/words.txt"
+        f = open(path, "r")
+        data = f.read().splitlines()
+        for word in data:
+            if len(word) == 5:
+                self.words.append(word)
+        f.close()
+        return self.words
+
+# this is used to pick a random word from the list of words
+    def word_picker(self, used_words):
+        try:
+            words = self.utils.file_transfer()
+            wordle = random.choice(words)
+            while wordle.upper() in used_words:
+                wordle = random.choice(words)
+            return wordle.upper()
+        except Exception as e:
+            print(f"Error: {e}")
---
+++ HW03_Shreya_Mohan_ui.py
@@ -0,0 +1,67 @@
+import HW06_Shreya_Mohan_utility as util
+
+
+class Info:
+
+    def __init__(self):
+        self.utils = util.Utility()
+
+    def __str__(self):
+        return f'Info: ({self.utils})'
+
+# accepts user input with input validations
+    def user(self, attempts, prev, all_words):
+        try:
+            wordle = input(f"Attempt #{7-attempts}\nEnter a 5 letter word \n")
+        except Exception as e:
+            self.utils.write_logs(f"ERROR : INPUT ERROR {e}", True)
+        self.utils.write_logs(f'USER ENTERED: "{wordle.upper()}"')
+        if not wordle:
+            print("Thank you for playing, exiting game now")
+            self.utils.write_logs(f"USER EXITED THE GAME")
+            quit()
+        # to convert all input to upper case
+        wordle = wordle.upper()
+        # to check if the input is not of length 5 and is not consisting of only alphabets
+        if len(wordle) != 5 or not wordle.isalpha():
+            self.utils.write_logs(
+                f"ERROR : USER ENTERED AN INVALID WORD", True)
+            return False, "Please enter a valid 5 letter word consisting of only alphabets."
+        if wordle.upper() not in all_words:
+            # print("Word is not in dictionary")
+            self.utils.write_logs(
+                f"ERROR : USER ENTERED A WORD WHICH IS NOT IN THE DICTIONARY", True)
+            return False, "Word is not in dictionary"
+        if wordle in prev:
+            # print("You have already entered this word.")
+            self.utils.write_logs(f"ERROR : USER ENTERED A USED WORD", True)
+            return False, "You have already entered this word."
+        # to check if the input is of length 5 and is consisting of only alphabets
+        if len(wordle) == 5 and wordle not in prev and wordle.isalpha():
+            return True, wordle.upper()
+
+# function for automation of wordle game using helper function
+    def automatic_attempts(self, attempts, wordle):
+        self.utils.write_logs(f'USER ENTERED: "{wordle.upper()}"')
+        if not wordle:
+            print("Thank you for playing, exiting game now")
+            self.utils.write_logs(f"USER EXITED THE GAME")
+            quit()
+        # to convert all input to upper case
+        wordle = wordle.upper()
+        # to check if the input is not of length 5 and is not consisting of only alphabets
+        if len(wordle) != 5 or not wordle.isalpha():
+            self.utils.write_logs(
+                f"ERROR : USER ENTERED AN INVALID WORD", True)
+            return False, "Please enter a valid 5 letter word consisting of only alphabets."
+        # if wordle.upper() not in all_words:
+        #     # print("Word is not in dictionary")
+        #     self.utils.write_logs(f"ERROR : USER ENTERED A WORD WHICH IS NOT IN THE DICTIONARY",True)
+        #     return False,"Word is not in dictionary"
+        # if wordle in prev:
+        #     # print("You have already entered this word.")
+        #     self.utils.write_logs(f"ERROR : USER ENTERED A USED WORD",True)
+        #     return False,"You have already entered this word."
+        # to check if the input is of length 5 and is consisting of only alphabets
+        if len(wordle) == 5 and wordle.isalpha():
+            return True, wordle.upper()
